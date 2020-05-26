c67f744f1aa091a2789e95c3530d1fdf7a01469e
unknown <sduarteh46@gmail.com>
2020-05-26 16:51:26 +0200

    Added Session Storage and Persistence to keep the shopping bag when refreshing the page

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import { Provider } from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
 
-import store from "./redux/store";
+import {store, persistor} from "./redux/store";
 
 
 import './index.css';
@@ -14,7 +15,9 @@ ReactDOM.render(
   <Provider store={store}>
   <React.StrictMode>
   <BrowserRouter>
+  <PersistGate persistor={persistor}>
   <App />
+  </PersistGate>
   </BrowserRouter>
   </React.StrictMode>,
   </Provider>,
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from "redux";
+import {persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+// import sessionStorage from "redux-persist/es/storage/session"
 
 import userReducer from "./user/user.reducer";
 import cartReducer from  "./cart/cart.reducer";
 
-export default combineReducers({
+const persistConfig = {
+    key:"root",
+    storage,
+    whitelist: ["cart"]
+}
+
+const rootReducer = combineReducers({
     user:userReducer,
     cart: cartReducer
 }); 
+
+export default persistReducer(persistConfig, rootReducer);
