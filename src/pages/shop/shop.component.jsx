101300a41322362ec37e26e18985509d4c865dec
unknown <sduarteh46@gmail.com>
2020-05-27 09:47:52 +0200

    Added redux to collections-overview componentts and shop-collection

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,16 @@
 import React from "react";
 
-import SHOP_DATA from "./shop.data";
-
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const {collections} = this.state;
-        return( <div className="shop-page" > 
-        {
-            collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id} {...otherCollectionProps}/>
-            ))
-        }
-         </div>
-        )
-    }
+import CollectionOverview from "../../components/collections-overview/collections-overview.component"
 
 
-}
+const ShopPage = ({collections}) => (
+
+
+        <div className="shop-page" > 
+            <CollectionOverview/>
+        </div>
+        
+);
+
 
 export default ShopPage;
